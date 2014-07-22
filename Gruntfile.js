421385d8b02fa765c413987f04040da55df85214
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-07-22 14:07:57 -0500

    Clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,48 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      options: {
-        jshintrc: true
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['js/app/**/*.js']
-      }
-    },
-    blanket_qunit: {
-      all: {
-        options: {
-          urls: ['test/spec.html?coverage=true&gruntReport'],
-          threshold: 70
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: {
+                src: ['js/app/**/*.js']
+            }
+        },
+        blanket_qunit: {
+            all: {
+                options: {
+                    urls: ['test/spec.html?coverage=true&gruntReport'],
+                    threshold: 70
+                }
+            }
+        },
+        plato: {
+            main: {
+                files: {
+                    'reports': ['js/app/**/*.js']
+                }
+            }
+        },
+        watch: {
+            qunit: {
+                files: ['test/*.js', 'js/app/**/*.js'],
+                tasks: ['jshint', 'blanket_qunit']
+            }
         }
-      }
-    },
-    plato: {
-      main: {
-        files: {
-          'reports': ['js/app/**/*.js']
-        }
-      }
-    },
-    watch: {
-      qunit: {
-        files: ['test/*.js', 'js/app/**/*.js'],
-        tasks: ['blanket_qunit']
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-plato');
-  grunt.loadNpmTasks('grunt-blanket-qunit');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-plato');
+    grunt.loadNpmTasks('grunt-blanket-qunit');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('test', ['blanket_qunit']);
-
+    // Default task.
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('test', ['blanket_qunit']);
 };
