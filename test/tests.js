8a0fceebf429247f6bb37fef246998234785cf9a
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-07-30 22:57:11 -0500

    A few more small specs

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,8 @@
 module("zip code searcher");
 
 // "should be a zip code search view model"
+// "should have fields for zip, city, state, county"
+// "should know when a zip is found"
 // "should validate zip codes"
 // "should check the length of zip codes"
 // "should search for zip codes if a zip is valid"
